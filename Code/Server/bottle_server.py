d110a02a437febad9413361456d3639dce86e317
Loruski <lorusso116@gmail.com>
2024-02-14 22:22:08 +0100

    implementation of prediction function in bottle server: callable by POST request to address /predict with body "text:input"

--- Code/Server/bottle_server.py
+++ Code/Server/bottle_server.py
@@ -1,19 +1,77 @@
-from bottle import route, run
+from bottle import run, route, get, post, request, FormsDict
 from joblib import load
-import sklearn.pipeline as Pipeline
+from sklearn.pipeline import Pipeline as Pipeline
 from NLTKVectorizer import NLTKVectorizer
+from numpy import ndarray
+from json import dumps as jsondump
+
+# decidere se avere cambiamenti del server in realtime mentre si modifica il .py (True)
+DEBUG = True
+
+# decidere se usare direttamente il modello presettato (True) o obbligare l'utente a caricare il modello
+CUSTOM = False
+model_pipeline: Pipeline = None if CUSTOM else load("../dump/model.joblib")
 
 NLTKVectorizer = NLTKVectorizer()
 
-model_Pipeline: Pipeline
+
+###################### metodi e classi di supporto
+def ndarray_to_dict(array: ndarray) -> dict:
+    i = 0
+    array = array[0]
+    dictionary = dict()
+    while i < len(array):
+        dictionary[i] = array[i]
+        i += 1
+    return dictionary
 
 
-@route("/load_server")
+###################### metodi per le richieste al server
+
+@route("/load_model")
 def load_server():
-    # model_Pipeline = load('../dump/model.joblib') #official model
-    model_Pipeline = load('../dump/test.joblib')  # test model
-    if model_Pipeline is not None:
+    global model_pipeline
+    model_Pipeline = load('../dump/model.joblib')  # official model
+    if model_pipeline is not None:
         return "model Loaded"
 
 
-run(host='localhost', port=8080, debug=True)
+@get("/predict_form")
+def predict_get():
+    global model_pipeline
+    if model_pipeline is None:
+        return "model not loaded"
+
+    return '''
+        <form action="/predict_form" method="post">
+            input: <input name="prediction" type="text" />
+            <input value="Predict" type="submit" />
+        </form>
+    '''
+
+
+# i metodi dichiarati sopra e sotto servono come debug
+@post("/predict_form")
+def predict_post():
+    global model_pipeline
+
+    submitted: FormsDict = request.forms.get('prediction')  # se mandato via forms
+
+    prediction = model_pipeline.predict_proba([submitted])
+
+    prediction_dict: dict = ndarray_to_dict(prediction)
+
+    return jsondump(prediction_dict)
+
+
+@post("/predict")  # predict ufficiale
+def predict_post():
+    global model_pipeline
+    submitted: FormsDict = request.get("text")
+
+    prediction = model_pipeline.predict_proba([submitted])
+
+    return str(prediction)
+
+
+run(host='localhost', port=8080, debug=True, reloader=True if DEBUG else None)
